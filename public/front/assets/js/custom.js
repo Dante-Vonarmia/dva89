4a0cb51bdbf14a2e681ae2656b1c6fb23672aac2
Dante_Von_Armia <dantestylish@hotmail.com>
2019-09-09 00:08:04 -0500

    compromise a bug, not my fault

--- public/front/assets/js/custom.js
+++ public/front/assets/js/custom.js
@@ -1,25 +1,22 @@
 /* ============================================================
- * Portfolio Init
+ * HomePage Init
  * ============================================================ */
 'use strict';
-$(function() {
 
-	//Intialize Slider
-	var slider = new Swiper('#hero', {
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true
-		},
-		slidesPerView: 1,
-		keyboard: {
-			enabled: true,
-		},
-		mousewheel: true,
-		paginationClickable: true,
-		nextButton: '.swiper-button-next',
-		prevButton: '.swiper-button-prev',
-		parallax: true,
-		speed: 1000
-	});
-
-})
+//Intialize Slider
+var slider = new Swiper('#hero', {
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+	slidesPerView: 1,
+	keyboard: {
+		enabled: true,
+	},
+	mousewheel: true,
+	paginationClickable: true,
+	nextButton: '.swiper-button-next',
+	prevButton: '.swiper-button-prev',
+	parallax: true,
+	speed: 1000
+});
